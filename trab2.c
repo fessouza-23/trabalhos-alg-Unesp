80effa3af9801d09288cd69dfa02aa078ad6913e
Eduardo R. Teixeira <teixeira.eduardo2001@gmail.com>
2023-06-22 13:02:23 -0300

    Update trab2.c

--- trab2.c
+++ trab2.c
@@ -4,7 +4,42 @@
 
 #define size 3
 #define player_symb 'X'
+#define player2_symb 'O'
 #define computer_symb 'O'
+
+void initBoard(char board[][size]);
+
+void printBoard(char board[][size]);
+
+int moveValidation(char board[][size], int row, int col);
+
+int checkWin(char board[][size], char symb);
+
+int checkBoardFull(char board[][size]);
+
+void playerMove(char board[][size]);
+
+void computerMove(char board[][size]);
+
+void initGame();
+
+void versusComputer();
+
+void versusHuman();
+
+void help();
+
+void about();
+
+void mainMenu();
+
+int main() {
+    mainMenu();
+
+    initGame();
+
+    return 0;
+}
 
 void initBoard(char board[][size]) {
     int i, j;
@@ -41,7 +76,7 @@ int moveValidation(char board[][size], int row, int col) {
     return 1;
 }
 
-int checkWin(char board[0][size], char symb) {
+int checkWin(char board[][size], char symb) {
     int i, j;
 
     // verificação por linha
@@ -90,6 +125,20 @@ void playerMove(char board[][size]) {
     board[row][col] = player_symb;
 }
 
+void player2Move(char board[][size]) {
+    int row, col;
+    printf("Sua vez de jogar (linha coluna): ");
+    scanf("%d %d", &row, &col);
+
+    if (!moveValidation(board, row, col)) {
+        printf("Movimento invalido! Tente novamente.\n");
+        playerMove(board);
+        return;
+    }
+
+    board[row][col] = player2_symb;
+}
+
 void computerMove(char board[][size]) {
     srand(time(NULL));
 
@@ -102,7 +151,30 @@ void computerMove(char board[][size]) {
     board[row][col] = computer_symb;
 }
 
-int main() {
+void initGame() {
+    printf("\e[?25h");
+    system("cls");
+
+    printf("Bem-vindo ao Jogo da Velha!\n\n");
+    printf("Aperte 1 para jogar contra a maquina ou 2 para jogar contra outro humano\n");
+
+    int numberOfPlayers = getch();
+
+    do {
+        if (numberOfPlayers == 49) {
+            versusComputer();
+        } else if (numberOfPlayers == 50) {
+            versusHuman();
+        } else {
+            printf("Valor inválido! Digite novamente.\n");
+        }
+    } while (numberOfPlayers != 49 && numberOfPlayers != 50);
+
+}
+
+void versusComputer() {
+    system("cls");
+
     char board[size][size];
     int gameOver = 0;
 
@@ -136,12 +208,137 @@ int main() {
         if (checkBoardFull(board)) {
             printf("Empate! O jogo terminou empatado.\n");
             gameOver = 1;
+            }
         }
-    }
-
+    system("cls");
     printBoard(board);
     printf("Fim do jogo!\n");
     system("pause");
+    mainMenu();
+}
 
-    return 0;
+void versusHuman() {
+    system("cls");
+
+    char board[size][size];
+    int gameOver = 0;
+
+    initBoard(board);
+
+    printf("Bem-vindo ao Jogo da Velha!\n\n");
+
+    while (!gameOver) {
+        printBoard(board);
+
+        playerMove(board);
+        if (checkWin(board, player_symb)) {
+            printf("Parabens! O Jogador 1 ganhou!\n");
+            gameOver = 1;
+            break;
+        }
+
+        if (checkBoardFull(board)) {
+            printf("Empate! O jogo terminou empatado.\n");
+            gameOver = 1;
+            break;
+        }
+
+        player2Move(board);
+        if (checkWin(board, player2_symb)) {
+            printf("Parabéns! O jogador 2 ganhou.\n");
+            gameOver = 1;
+            break;
+        }
+
+        if (checkBoardFull(board)) {
+            printf("Empate! O jogo terminou empatado.\n");
+            gameOver = 1;
+            }
+        }
+    system("cls");
+    printBoard(board);
+    printf("Fim do jogo!\n");
+    system("pause");
+    mainMenu();
+}
+
+void help(){
+    system("cls");
+    printf("-------------------------------\n");
+    printf("| JOGO DA VELHA - REGRAS      |\n");
+    printf("| O primeiro movimento e do X |\n");
+    printf("|Vence quem fechar uma linha  |\n");
+    printf("|coluna ou diagonal            |\n");
+    printf("-------------------------------\n");
+    getch();
+    mainMenu();
+}
+
+void about() {
+    system("cls");
+    printf("Programa desenvolvido por Eduardo e Joao para a disciplina de Algoritmos II\n");
+    printf("BSI 2023\n");
+    system("pause");
+    mainMenu();
+}
+
+void mainMenu() {
+    system("cls");
+    printf("\e[?25l");
+
+    int position = 1;
+    int keyPressed = 0;
+
+    while (keyPressed != 13) {
+        system("cls");
+        printf("  ---------------------------------------------\n");
+        printf("  | JOGO DA VELHA - Criado por Eduardo e Joao |\n");
+
+        arrowSelection(1, position); printf("| Iniciar                                   |\n");
+        arrowSelection(2, position); printf("| Ajuda                                     |\n");
+        arrowSelection(3, position); printf("| Sobre                                     |\n");
+        arrowSelection(4, position); printf("| Sair                                      |\n");
+
+        printf("  ---------------------------------------------\n");
+
+        keyPressed = getch();
+
+        if (keyPressed == 80 && position != 4) {
+            position++;
+        } else if (keyPressed == 72 && position != 1) {
+            position--;
+        } else if (keyPressed == 27){
+            system("cls");
+            printf(" ------------------- \n");
+            printf("|Obrigado por jogar!|\n");
+            printf(" ------------------- \n");
+            getch();
+            exit(0);
+        } else {
+            position = position;
+        }
+    }
+
+    if (position == 1) {
+        initGame();
+    } else if (position == 2) {
+        help();
+    } else if (position == 3) {
+        about(); 
+    } else if (position == 4) {
+        system("cls");
+        printf(" ------------------- \n");
+        printf("|Obrigado por jogar!|\n");
+        printf(" ------------------- \n");
+        getch();
+        exit(0);
+    }
+}
+
+void arrowSelection(int actualPos, int arrowPos) {
+    if (actualPos == arrowPos) {
+        printf(">>");
+    } else {
+        printf("  ");
+    }
 }
