3f1cdc1cf0085a79883cd86d958aec54a31efe0d
fessouza-dev <joao.fessouza@gmail.com>
2023-09-09 14:46:07 -0300

    Update - renomea ALGORITMOS 2

---
+++ Algoritmos II/funcs.h
@@ -0,0 +1,46 @@
+#ifndef FUNCS_H
+#define FUNCS_H
+
+#define esc 27
+#define f1 59
+
+typedef struct {
+    int  codigo;
+    char nome[50];
+    int  idade;
+    char endereco[50];
+    char fone[21];
+} Cliente;
+
+typedef struct {
+    int  codigo;
+    int  existe;
+    char nomeDoCliente[50];
+    int  dia;
+    int  mes; 
+    int  ano;
+} Consulta;
+
+FILE *abrirArquivo(const char nome_arquivo[20], const char op[5]);
+
+int confirma();
+
+void sobre();
+
+void cadastrarCliente();
+
+void consultarCliente();
+
+void atualizarCliente();
+
+void marcarConsulta();
+
+void desmarcarConsulta();
+
+void listarConsultas();
+
+void desmarcarConsultaRemFis();
+
+void menu();
+
+#endif
