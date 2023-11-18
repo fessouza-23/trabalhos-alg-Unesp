6b1d796f4a814da751f36f1f1386cc75ae7c39a9
Eduardo R. Teixeira <teixeira.eduardo2001@gmail.com>
2023-11-18 14:11:34 -0300

    Add files via upload

---
+++ Algoritmos II/sortingViewer/main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include "sorting.h"
+#include "arrayAndFileManipulation.h"
+
+#ifdef _WIN32
+    #include <windows.h>
+#else
+    #include <unistd.h>
+#endif
+
+// Sorting Viewer - Visualizador de dados para algoritmos de ordenação                                       
+
+int main(int argc, char *argv[]) {
+    CLR_SCREEN;
+
+    int vet[MAX_ARRAY_SIZE], n = 0, retry, comps, trocas, arrayCompsAndTrocas[MAX_ARGS];
+
+    SortFunction bufferAlgos[] = {bubble_sort, insertion_sort, selection_sort, shell_sort};
+
+    SortFunctionSpecial bufferAlgosSpecial[] = {quick_sort, merge_sort}; 
+
+    const char *sortName[] = {"BUBBLE SORT", "INSERTION SORT", "SELECTION SORT", "SHELL SORT", "QUICK SORT", "MERGE SORT"}; 
+
+    printf("------------------------------------------------------------\n");
+    printf("| Visualizador de performance para algoritmos de ordenacao |\n");
+    printf("------------------------------------------------------------\n\n");
+
+    #ifdef __GNUC__
+        printf ("COMPILED ON GCC VERSION %d.%d\n\n", __GNUC__, __GNUC_MINOR__);
+    #endif
+
+    printf("Pressione qualquer tecla para continuar... ");
+    getchar();
+
+    writeFileInArray(vet);
+
+    do {
+
+        insertArraySize(&n);
+
+        printArrayData(bufferAlgos, bufferAlgosSpecial, vet, n, &comps, &trocas, arrayCompsAndTrocas, sortName);
+
+        runAgain(&retry);
+
+    } while (retry == '1');
+
+    exitProgram();
+
+    return 0;
+}
